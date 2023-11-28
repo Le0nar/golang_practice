a2150c103847629dbf1d7b31b5af7eb1633b9433
Le0nar <yaroslavtrefilov5@gmail.com>
2023-11-28 17:19:32 +0400

    feat: add read file by line

--- file/readfile/main.go
+++ file/readfile/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
 	"log"
 	"os"
 )
 
 func main() {
+	// Read the entire file
 	readTicketFile()
+	
+	// Read file by line
+	readTicketFileByline()
 }
 
 func readTicketFile() {
@@ -21,6 +27,22 @@ func readTicketFile() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	os.Stdout.Write(data)
+}
+
+func readTicketFileByline() {
+	file, err := os.Open("../createfile/tickets/Petr Petrov_0.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	fileScanner := bufio.NewScanner(file)
+ 
+    fileScanner.Split(bufio.ScanLines)
+  
+    for fileScanner.Scan() {
+        fmt.Println(fileScanner.Text())
+    }
 }
