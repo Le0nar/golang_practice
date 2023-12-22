a9f65a39714bcd0b33d34c9a913202ce165cf380
Le0nar <yaroslavtrefilov5@gmail.com>
2023-12-23 02:28:37 +0400

    feat: add seveth task

---
+++ 7_concurrancy_write_map.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func main() {
+	// TODO: look for better solution
+	var mux sync.Mutex
+	
+	object := make(map[int]int)
+
+	ch := make(chan [2]int)
+
+	go seedRand(ch, 0)
+	go seedRand(ch, 1)
+
+	for v := range ch {
+		key, value := v[0], v[1]
+
+		mux.Lock()
+		object[key] = value
+		mux.Unlock()
+
+		fmt.Printf("object: %v\n", object)
+	}
+
+}
+
+func seedRand(ch chan [2]int, value int) {
+	for {
+		// timeout is optional
+		time.Sleep(time.Millisecond * 500)
+
+		key := rand.Intn(10)
+		ch <- [2]int{key,value}
+	}
+}
