b22ae1273749d10dcec94beffedbb95fc7be6cac
Le0nar <yaroslavtrefilov5@gmail.com>
2023-12-22 17:53:39 +0400

    feat: add fourth task

---
+++ 4_channel.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+const workersQuantity = 3
+
+func main() {
+	quit := make(chan os.Signal)
+    signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	intChan := make(chan int)
+
+	var wg sync.WaitGroup
+	wg.Add(workersQuantity)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	for i := 0; i < workersQuantity; i++ {
+		go startWorker(ctx, &wg, intChan)
+	}
+
+	go seedRandInt(intChan)
+
+	<-quit
+	cancel()
+
+	wg.Wait()
+
+	fmt.Printf("The program exits")
+}
+
+func startWorker(ctx context.Context, wg *sync.WaitGroup, ch chan int) {
+	for {
+		select {
+		case value := <- ch:
+			fmt.Printf("value: %v\n", value)
+			
+		case <- ctx.Done():
+			fmt.Printf("Worker finishes work \n")
+			wg.Done()
+			return
+		}
+	}
+}
+
+func seedRandInt(ch chan int) {
+	for {
+		// TODO: timeout is optional
+		time.Sleep(time.Millisecond * 500)
+
+		ch <- rand.Intn(100)
+	}
+}
