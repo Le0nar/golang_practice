e351e1f79b4ec19b22a3600f3452fdab81f6a491
Le0nar <yaroslavtrefilov5@gmail.com>
2023-12-30 15:40:55 +0400

    feat: add 11 task

--- 11_set.go
+++ 11_set.go
@@ -1,1 +1,26 @@
-// TODO: implement it
+package main
+
+import "fmt"
+
+func main() {
+	femaleNames := []string{"Саша", "Катя", "Женя", "Вика"}
+	maleNames := []string{"Саша", "Витя", "Женя", "Кузя"}
+
+	intersections := getIntersection(femaleNames, maleNames)
+
+	fmt.Printf("intersections: %v\n", intersections)
+}
+
+func getIntersection[T comparable](firstList, secondList []T) []T {
+	intersections := make([]T, 0)
+
+	for _, firstListValue := range firstList {
+		for _, secondListValue := range secondList {
+			if firstListValue == secondListValue {
+				intersections = append(intersections, secondListValue)
+			}
+		}
+	}
+
+	return intersections
+}
