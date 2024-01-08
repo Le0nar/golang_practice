8a85f8d18533b27a648e6e5002fcfbf2ab1797d0
Le0nar <yaroslavtrefilov5@gmail.com>
2024-01-08 23:16:34 +0400

    fix: remove buffer from task 9

--- 9_channels.go
+++ 9_channels.go
@@ -1,32 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
+	var wg sync.WaitGroup
+	wg.Add(3)
+
 	originList := []int{1, 2,3,4,5,6,7,8,9,10}
-
 	firstCh := make(chan int)
-	bufSize := len(originList) - 1
-	secondCh := make(chan int, bufSize)
+	secondCh := make(chan int)
 
-	go start(firstCh, secondCh, len(originList))
+	go start(firstCh, secondCh, len(originList), &wg)
 
-	for _, v := range originList {
-		firstCh <- v
-	}
+	go func ()  {
+		for _, v := range originList {
+			firstCh <- v
+		}
+		wg.Done()
+	}()
 
-	for i := 0; i < len(originList); i++ {
-		value := <- secondCh
-		fmt.Printf("value: %v\n", value)
-	}
+	go func ()  {
+		for i := 0; i < len(originList); i++ {
+			value := <- secondCh
+			fmt.Printf("value: %v\n", value)
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
 
 // TODO: add closing for goroutine
 // TODO: mb make first chan only for read and second only for write
-func start(readCh, writeCh chan int, listLength int) {
+func start(readCh, writeCh chan int, listLength int, wg *sync.WaitGroup) {
 	for i := 0; i < listLength; i++ {
 		value := <- readCh
 		value *= 2
 		writeCh <- value
 	}
+
+	wg.Done()
 }
